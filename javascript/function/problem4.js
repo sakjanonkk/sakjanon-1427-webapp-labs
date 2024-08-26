ae0f5701c71fc4f0fa90e0e672c4ae4f7fcf386f
sakjanonkk <sakjanon.k@kkumail.com>
2024-08-26 16:31:43 +0700

    ex14

--- javascript/function/problem4.js
+++ javascript/function/problem4.js
@@ -1,12 +1,16 @@
-const celsiusToFahrenheit = (celsius) => ((celsius * 9) / 5 + 32).toFixed(2);
+const celsiusToFahrenheit = (celsius) => {
+  return ((celsius * 9) / 5 + 32).toFixed(2);
+};
 
-const fahrenheitToCelsius = (fahrenheit) =>
-  (((fahrenheit - 32) * 5) / 9).toFixed(2);
+const fahrenheitToCelsius = (fahrenheit) => {
+  return (((fahrenheit - 32) * 5) / 9).toFixed(2);
+};
 
-const formatConversion = (temp, convert, unit) =>
-  `${temp.toFixed(2)} ${unit} is ${convert(temp)} ${
-    unit === "°C" ? "°F" : "°C"
-  }`;
+const formatConversion = (temp, convert, unit) => {
+  let convertedTemp = convert(temp);
+  let newUnit = unit === "°C" ? "°F" : "°C";
+  return temp.toFixed(2) + " " + unit + " is " + convertedTemp + " " + newUnit;
+};
 
 console.log(formatConversion(20, celsiusToFahrenheit, "°C"));
 console.log(formatConversion(98.6, fahrenheitToCelsius, "°F"));
