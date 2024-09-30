59103483c7d6624a29912c48b46a00b74bc43529
sakjanonkk <sakjanon.k@kkumail.com>
2024-09-30 16:33:47 +0700

    [+] Fix some code for AXIOS and ExpressJS

--- express/ip-api.js
+++ express/ip-api.js
@@ -7,11 +7,9 @@ const API_URL = "https://httpbin.org/ip"; // URL to get the IP address
 
 app.get("/ip", async (req, res) => {
   try {
-    //Call the httpbin API to get the IP address
     const response = await axios.get(API_URL);
-    const ip = response.data.origin; // Extract IP from the response
+    const ip = response.data.origin;
 
-    //IP address in JSON format
     res.json({ ip });
   } catch (error) {
     console.error("Error fetching the IP address:", error.message);
